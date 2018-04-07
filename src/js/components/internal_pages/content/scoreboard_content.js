b5e565e9eae1c3a6d961ec90e978efe5bc546080
Guy Paterson-Jones <guy@luno.com>
2018-04-08 01:20:48 +0200

    Abstracted out card code.

--- src/js/components/internal_pages/content/scoreboard_content.js
+++ src/js/components/internal_pages/content/scoreboard_content.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import DataTable from '../../data_table.js';
+import { DataTable } from '../../data_table.js';
 import DropDown from '../../drop_down.js';
 
 export default class ScoreboardContent extends React.Component {
     render() {
-        return <main className='scoreboard-content'>
+        return <div className='scoreboard-content'>
             Category: <DropDown/>
 
             <DataTable/>
-        </main>
+        </div>
     }
 }
